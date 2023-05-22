611d890e962abdb5ad28aa1ca6d3195dea51f236
fiorafabio <108327824+fiorafabio@users.noreply.github.com>
2023-05-22 17:02:05 +0200

    redis

--- rocketDB/src/main/java/com/f2/RocketDbApplication.java
+++ rocketDB/src/main/java/com/f2/RocketDbApplication.java
@@ -2,12 +2,20 @@ package com.f2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import redis.clients.jedis.JedisPooled;
 
 @SpringBootApplication
 public class RocketDbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RocketDbApplication.class, args);
+	}
+	
+	@Bean
+	JedisPooled jedisPooled() {
+		return new JedisPooled("192.168.1.234", 6379, "pablo", "ciaogamer123");
 	}
 
 }
